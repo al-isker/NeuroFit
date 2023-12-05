754242e006f247e94788fdd2e002949e6705a492
al-isker <139923683+al-isker@users.noreply.github.com>
2023-12-05 12:47:45 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// const path = document.querySelector('.path');
+// const pathH2 = path.querySelector('h2');
+// const pathImg = path.querySelector('img');
+//
+// document.addEventListener('scroll', function() {
+//     // const dpcOffsetTop = window.
+//
+//     const offsetTop =  path.offsetTop;
+//     const clientHeight = path.clientHeight;
+//
+//     // if(offsetTop + clientHeight/2 > )
+//
+//     console.log(offsetTop, clientHeight);
+// })
